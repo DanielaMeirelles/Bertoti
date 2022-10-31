5bb6f9615795f4207fbbd9dc2b44e5aa47a8a876
DanielaMeirelles <71828460+DanielaMeirelles@users.noreply.github.com>
2022-10-31 10:28:32 -0300

    Delete Engenharia 3/Observer/pattern/src/main directory

--- Engenharia 3/Observer/pattern/src/main/ListaEventos.java
+++
@@ -1,6 +0,0 @@
-package main;
-
-public interface ListaEventos {
-	public void update(String message);
-
-}
--- Engenharia 3/Observer/pattern/src/main/OuvinteTemperatura.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class OuvinteTemperatura implements ListaEventos {
-	public void update (String message) {
-		System.out.println(message + "|> para o ouvinte da Previsao de Temperatura");
-	}
-
-}
